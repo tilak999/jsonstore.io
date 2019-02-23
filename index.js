517d227d68efb26b3ee73032e4aac5acbcc94eb7
Naman Gupta <naman.gupta@innoplexus.com>
2019-02-23 13:41:11 +0530

    code refactoring and error handling

--- index.js
+++ index.js
@@ -1,87 +1,86 @@
-let fetch = require('fetch').fetchUrl
+let fetch = require('fetch').fetchUrl;
 
-const url = 'https://www.jsonstore.io/'
+const url = 'https://www.jsonstore.io/';
 
 class jsonstore {
-  constructor(token){
-    if (typeof(token) === 'undefined' || token === '') {
-      console.log(new Error("Invalid Token, get token from https://www.jsonstore.io/"))
+  constructor(token) {
+    if (!token) {
+      console.error(
+        new Error('Invalid Token, get token from https://www.jsonstore.io/'),
+      );
     }
-    this.end_url = url + token
+    this.end_url = token ? url + token : url;
   }
 
-  read(node){
-    let url = this.end_url
-    if (typeof(node) !== 'undefined') url = url + '/' + node
+  read(node) {
+    let url = this.end_url;
+    url = node ? url + '/' + node : url;
     return new Promise((resolve, reject) => {
-      let config = {
+      const config = {
         headers: {
-          'Content-type': 'application/json'
+          'Content-type': 'application/json',
         },
         method: 'GET',
-      }
+      };
       fetch(url, config, (error, meta, body) => {
-        if(!error){
+        if (!error) {
           try {
-            let response = JSON.parse(body.toString())
-            if (response.ok) resolve(response.result)
+            const response = JSON.parse(body.toString());
+            if (response.ok) resolve(response.result);
           } catch (err) {
-            reject(new Error("Token is incorrect, unable to read data."))
+            reject(new Error('Token is incorrect, unable to read data.'));
           }
-        }
-        else reject(new Error("Bad request, couldn't read data"))
-      })
-    })
+        } else reject(new Error("Bad request, couldn't read data"));
+      });
+    });
   }
 
-  write(node, payload){
-    let url = this.end_url
-    if (typeof(node) !== 'undefined' && node !== '') url = url + '/' + node
+  write(node, payload) {
+    let url = this.end_url;
+    url = node ? url + '/' + node : url;
     return new Promise((resolve, reject) => {
-      let config = {
+      const config = {
         headers: {
-          'Content-type': 'application/json; charset=utf-8'
+          'Content-type': 'application/json; charset=utf-8',
         },
         method: 'POST',
-        payload : JSON.stringify(payload)
-      }
+        payload: JSON.stringify(payload),
+      };
       fetch(url, config, (error, meta, body) => {
-        if(!error){
+        if (!error) {
           try {
-            let response = JSON.parse(body.toString())
-            if (response.ok) resolve(response.ok)
+            const response = JSON.parse(body.toString());
+            if (response.ok) resolve(response.ok);
           } catch (err) {
-            reject(new Error("Token is incorrect, unable to read data."))
+            reject(new Error('Token is incorrect, unable to read data.'));
           }
-        }
-        else reject(new Error("Bad request, couldn't read data"))
-      })
-    })
+        } else reject(new Error("Bad request, couldn't read data"));
+      });
+    });
   }
 
-  delete(node){
-    let url = this.end_url
-    if (typeof(node) !== 'undefined' && node !== '') url = url + '/' + node
+  delete(node) {
+    let url = this.end_url;
+    url = node ? url + '/' + node : url;
     return new Promise((resolve, reject) => {
-      let config = {
+      const config = {
         headers: {
-          'Content-type': 'application/json; charset=utf-8'
+          'Content-type': 'application/json; charset=utf-8',
         },
-        method: 'DELETE'
-      }
+        method: 'DELETE',
+      };
       fetch(url, config, (error, meta, body) => {
-        if(!error){
+        if (!error) {
           try {
-            let response = JSON.parse(body.toString())
-            if (response.ok) resolve(response.ok)
+            const response = JSON.parse(body.toString());
+            if (response.ok) resolve(response.ok);
           } catch (err) {
-            reject(new Error("Token is incorrect, unable to read data."))
+            reject(new Error('Token is incorrect, unable to read data.'));
           }
-        }
-        else reject(new Error("Bad request, couldn't read data"))
-      })
-    })
+        } else reject(new Error("Bad request, couldn't read data"));
+      });
+    });
   }
 }
 
-module.exports = jsonstore
+module.exports = jsonstore;
